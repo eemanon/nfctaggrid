a48cd23ffbca773e23c8c82e1f5c582bd3893f62
eemanon <bachti060293@gmail.com>
2021-06-02 10:51:48 +0200

    added layout elements for calibration

--- app/src/main/java/com/example/nfctaggrid/Accelerometer.java
+++ app/src/main/java/com/example/nfctaggrid/Accelerometer.java
@@ -6,10 +6,15 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Accelerometer extends AppCompatActivity {
 
     private SensorManager mSensorManager;
@@ -21,10 +26,20 @@ public class Accelerometer extends AppCompatActivity {
     private TextView txt_thresholdY;
     private TextView txt_thresholdZ;
 
+    private TextView txt_smoothedX;
+    private TextView txt_smoothedY;
+    private TextView txt_smoothedZ;
 
     private TabLayout tabLayout;
+
+    Queue<Float> samplesX;
+    Queue<Float> samplesY;
+    Queue<Float> samplesZ;
+    int bufferLength;
     SensorEventListener acc;
     private float[] thresholds = {0,0,0};
+    private boolean calibration = true;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +53,18 @@ public class Accelerometer extends AppCompatActivity {
         txt_thresholdX = (TextView) findViewById(R.id.txt_threshold_x);
         txt_thresholdY = (TextView) findViewById(R.id.txt_threshold_y);
         txt_thresholdZ = (TextView) findViewById(R.id.txt_threshold_z);
+        txt_smoothedX = (TextView) findViewById(R.id.txt_filterX);
+        txt_smoothedY = (TextView) findViewById(R.id.txt_filterY);
+        txt_smoothedZ = (TextView) findViewById(R.id.txt_filterZ);
+        btn = findViewById(R.id.btn_calibration);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calibration = false;
+            }
+        });
+
         tabLayout = findViewById(R.id.tablayout);
         tabLayout.getTabAt(1).select();
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -68,15 +95,39 @@ public class Accelerometer extends AppCompatActivity {
                     txt_x.setText(""+event.values[0]);
                     txt_y.setText(""+event.values[1]);
                     txt_z.setText(""+event.values[2]);
-                    if (Math.abs(event.values[0])>thresholds[0])
-                        thresholds[0] = Math.abs(event.values[0]);
-                    if (Math.abs(event.values[1])>thresholds[1])
-                        thresholds[1] = Math.abs(event.values[1]);
-                    if (Math.abs(event.values[2])>thresholds[2])
-                        thresholds[2] = Math.abs(event.values[2]);
-                    txt_thresholdX.setText(""+thresholds[0]);
-                    txt_thresholdY.setText(""+thresholds[1]);
-                    txt_thresholdZ.setText(""+thresholds[2]);
+                    if (calibration){
+                        if (Math.abs(event.values[0])>thresholds[0])
+                            thresholds[0] = Math.abs(event.values[0]);
+                        if (Math.abs(event.values[1])>thresholds[1])
+                            thresholds[1] = Math.abs(event.values[1]);
+                        if (Math.abs(event.values[2])>thresholds[2])
+                            thresholds[2] = Math.abs(event.values[2]);
+                        txt_thresholdX.setText(""+thresholds[0]);
+                        txt_thresholdY.setText(""+thresholds[1]);
+                        txt_thresholdZ.setText(""+thresholds[2]);
+                    } else {
+                        samplesX.remove();
+                        samplesY.remove();
+                        samplesZ.remove();
+                        samplesX.add(event.values[0]);
+                        samplesY.add(event.values[1]);
+                        samplesZ.add(event.values[2]);
+                        float avgX = avg(samplesX);
+                        float avgY = avg(samplesY);
+                        float avgZ = avg(samplesZ);
+                        if(Math.abs(avgX)>thresholds[0]){
+                            txt_smoothedX.setText(Float.toString(avgX));
+                        } else
+                            txt_smoothedX.setText("0");
+                        if(Math.abs(avgY)>thresholds[1]){
+                            txt_smoothedY.setText(Float.toString(avgY));
+                        } else
+                            txt_smoothedY.setText("0");
+                        if(Math.abs(avgZ)>thresholds[2]){
+                            txt_smoothedZ.setText(Float.toString(avgZ));
+                        } else
+                            txt_smoothedZ.setText("0");
+                    }
                 }
             }
 
@@ -85,8 +136,32 @@ public class Accelerometer extends AppCompatActivity {
 
             }
         };
-        mSensorManager.registerListener(acc, mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), 1000);
+
+        samplesX = new LinkedList<Float>();
+        samplesY = new LinkedList<Float>();
+        samplesZ = new LinkedList<Float>();
+        bufferLength = 5;
+        //init queue
+        for(int i = 0;i<bufferLength;i++){
+            samplesX.add(0.0f);
+            samplesY.add(0.0f);
+            samplesZ.add(0.0f);
+        }
+
+        mSensorManager.registerListener(acc, mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), 10000);
+
+
     }
+
+    private float avg(Queue<Float> samples) {
+        float sum = 0.0f;
+        for(float number : samples){
+            sum+=number;
+        }
+        return sum/samples.size();
+    }
+
+
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(acc);
